e68261742656e74c3e3a8b5fd1e85640110b70a3
AHLASaad <93229038+AHLASaad@users.noreply.github.com>
2022-06-06 13:14:05 +0100

    Update network_discovery.ps1

--- network_discovery.ps1
+++ network_discovery.ps1
@@ -3,3 +3,4 @@ param($ip_network, $ip_range)
 $ping = New-Object System.Net.NetworkInformation.Ping
 
 $ip_range | ForEach-Object {$ping.Send("$ip_network$_",1000)} | Select-Object Address,Status
+#.\netDisc.ps1 -ip_network 192.168.1. -ip_range (9..13) | Format-List
